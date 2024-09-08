04767d4e330a6221878688fd94eca47d6dd8e263
Yuri Canuto <yuriasc@gmail.com>
2024-09-08 02:00:39 -0300

    Criado menu inicial

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-3">
+                <div class="card">
+                    <div class="card-header">{{ __('Menu') }}</div>
+                    <div class="card-body">
+                        <ul class="nav nav-pills flex-column mb-auto">
+                            <li class="nav-item">
+                                <a href="#" class="nav-link {{ request()->routeIs('home') ? 'active' : '' }}" aria-current="page">
+                                    <i class="bi bi-house me-2"></i>
+                                    Home
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="nav-link">
+                                    <svg class="bi pe-none me-2" width="16" height="16">
+                                        <use xlink:href="#speedometer2"></use>
+                                    </svg>
+                                    Dashboard
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            <div class="col-md-9">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
