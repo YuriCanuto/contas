5b34c167dd3cd975ab8ffe6e4aee11d550a06c9d
Yuri Canuto <yuriasc@gmail.com>
2025-07-21 00:25:13 -0300

    Ajuste nas rotas para usarem o middleware auth
    Na rota de logout ajuste no redirectTo

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -25,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/login';
 
     /**
      * Create a new controller instance.
@@ -36,5 +38,25 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
         $this->middleware('auth')->only('logout');
+    }
+
+    /**
+     * Log the user out of the application.
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        if ($response = $this->loggedOut($request)) {
+            return $response;
+        }
+
+        return $request->wantsJson()
+            ? new JsonResponse([], 204)
+            : redirect($this->redirectTo);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Cards\DeleteCardController;
 use App\Http\Controllers\Cards\EditarCardController;
 use App\Http\Controllers\Cards\ListarCardsController;
@@ -9,19 +10,18 @@ use App\Http\Controllers\Cards\UpdateCardController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+
+    Route::prefix('cards')->name('cards.')->group(function () {
+        Route::get('/', ListarCardsController::class)->name('listar');
+        Route::get('/novo', NovoCardController::class)->name('novo');
+        Route::post('/create', StoreCardController::class)->name('store');
+        Route::get('/editar/{card_id}', EditarCardController::class)->name('editar');
+        Route::put('/update/{card_id}', UpdateCardController::class)->name('update');
+        Route::delete('/delete/{card_id}', DeleteCardController::class)->name('delete');
+    });
 });
 
 Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Route::prefix('cards')->name('cards.')->group(function () {
-    Route::get('/', ListarCardsController::class)->name('listar');
-    Route::get('/novo', NovoCardController::class)->name('novo');
-    Route::post('/create', StoreCardController::class)->name('store');
-    Route::get('/editar/{card_id}', EditarCardController::class)->name('editar');
-    Route::put('/update/{card_id}', UpdateCardController::class)->name('update');
-    Route::delete('/delete/{card_id}', DeleteCardController::class)->name('delete');
-});
