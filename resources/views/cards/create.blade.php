ed240903d94fa6b8541b862ce063a12ea0f1f075
Yuri Canuto <yuriasc@gmail.com>
2025-08-07 23:32:55 -0300

    Ajustes no layout do menu

--- resources/views/cards/create.blade.php
+++ resources/views/cards/create.blade.php
@@ -3,7 +3,9 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            @include('layouts.menu')
+            <div class="col-md-3">
+                @include('layouts.menu')
+            </div>
             <div class="col-md-9">
                 <div class="card">
                     <div class="card-header d-flex flex-row justify-content-between align-items-center">
